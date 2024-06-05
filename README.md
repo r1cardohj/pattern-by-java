aaf227871a5c06f093a5353b03aba42114b83e92
boxbox7 <houjun447@gmail.com>
2024-06-05 21:25:34 +0800

    Add observer pattern

---
+++ README.md
@@ -0,0 +1,3 @@
+pattern-by-java
+
+* observer
---
+++ src/observer/CurrentConditionsDisplay.java
@@ -0,0 +1,25 @@
+package observer;
+
+public class CurrentConditionsDisplay implements Observer, DisplayAble{
+    private float temperature;
+    private float humidity;
+    private WeatherData weatherData;
+
+    public CurrentConditionsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Current conditions: " + temperature
+            + "F degree and " + humidity + "% humidity");
+    }
+
+    @Override
+    public void update(float temp, float humidity, float pressure) {
+        this.temperature = temp;
+        this.humidity = humidity;
+        display();
+    }
+}

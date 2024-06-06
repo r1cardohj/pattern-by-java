a4766072eadd442da3541c199445f55ad7930531
r1cardohj <houjun447@gmail.com>
2024-06-07 00:05:33 +0800

    add observer class: AvgMaxMinTempDisplay

---
+++ src/observer/AvgMaxMinTempDisplay.java
@@ -0,0 +1,40 @@
+package observer;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class AvgMaxMinTempDisplay implements Observer, DisplayAble{
+    WeatherData weatherData;
+    List<Float> temperatures;
+    float maxTemperature;
+    float minTemperature;
+    float avgTemperature;
+
+
+    public AvgMaxMinTempDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        this.temperatures = new ArrayList<>();
+        weatherData.registerObserver(this);
+    }
+    @Override
+    public void update() {
+        temperatures.add(this.weatherData.getTemperature());
+        this.maxTemperature = Collections.max(temperatures);
+        this.minTemperature = Collections.min(temperatures);
+        float sum = 0;
+        for (float temp : temperatures) {
+            sum += temp;
+        }
+        this.avgTemperature = sum / temperatures.size();
+        this.display();
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("Avg/Max/Min temperature = %.1f/%.1f/%.1f%n",
+                this.avgTemperature, this.maxTemperature, this.minTemperature);
+    }
+}
--- src/observer/WeatherStation.java
+++ src/observer/WeatherStation.java
@@ -7,6 +7,7 @@ public class WeatherStation {
         CurrentConditionsDisplay currentDisplay =
                 new CurrentConditionsDisplay(weatherData);
         BetterDisplay betterDisplay = new BetterDisplay(weatherData);
+        AvgMaxMinTempDisplay avgMaxMinTempDisplay = new AvgMaxMinTempDisplay(weatherData);
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
         weatherData.setMeasurements(78, 90, 29.2f);
